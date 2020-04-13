d223cbbd2ff5d66bedaf205a8a164c0bd9823a3d
talgat55 <lightxdesign55@gmail.com>
2020-04-13 22:48:03 +0600

    import form in root

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import React from 'react';
-import Store from './Store';
+import Form from './components/Form';
 import {decorate, observable, action, computed} from 'mobx';
+import Store from './Store';
 
 
-decorate(Store, {
-    reviewList: observable,
-    addReview: action,
-    averageScore: computed,
-    reviewCount: computed
-});
-
+// decorate(Store, {
+//     reviewList: observable,
+//     addReview: action,
+//     averageScore: computed,
+//     reviewCount: computed
+// });
+const reviewStore = new Store();
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+          <Form store={reviewStore}/>
+      </div>
   );
 }
 
